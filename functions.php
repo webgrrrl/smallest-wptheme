d2dd592fe1443a2edf901f6b69a5139a21800b44
Lorna Timbah <me@webgrrrl.net>
2018-11-17 05:24:54 +0800

    First commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?
+if(!is_admin()){
+    function load_smallest_wptheme() {
+        wp_deregister_style('bootstrap');
+        wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+        wp_enqueue_style('bootstrap');
+        wp_deregister_script('bootstrap');
+        wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array(), false, true);
+        wp_enqueue_script('bootstrap');
+        wp_enqueue_script('jquery', '', array(), false, true);
+    }
+    add_action('init', 'load_smallest_wptheme');
+}
+?>
